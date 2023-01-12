6c0d30d3ff054c02df64723a790c8149a0bdf21b
vcatafesta <vcatafesta@gmail.com>
2023-01-11 20:00:40 -0400

    qua 11 jan 2023 20:00:40 -04 Vilmar Catafesta (vcatafesta@gmail.com)

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#sh <(curl -L https://raw.githubusercontent.com/voidlinux-br/void-installer/master/install.sh)
+#sh <(wget https://raw.githubusercontent.com/voidlinux-br/void-installer/master/install.sh)
+
+{
+# Previne execucao se foi baixado apenas parcialmente
+oops() {
+    echo "$0:" "$@" >&2
+    exit 1
+}
+
+umask 0022
+url='https://raw.githubusercontent.com/voidlinux-br/void-installer/master'
+files=('ChangeLog' 'INSTALL' 'LICENSE' 'MAINTAINERS' 'Makefile' 'README' 'README.md' 'void-install')
+tmpDir='~/github/void-installer'
+result="$(mkdir -p $tmpDir || oops "Não é possível criar diretório temporário para baixar arquivos")"
+
+require_util() {
+	command -v "$1" > /dev/null 2>&1 ||
+		oops "você não tem '$1' instalado, que eu preciso para $2"
+}
+
+#require_util tar "descompatar o tarball"
+
+if command -v curl > /dev/null 2>&1; then
+	cmdfetch() { curl --silent --insecure --fail -L "$1" -o "$2"; }
+elif command -v wget > /dev/null 2>&1; then
+	cmdfetch() { wget "$1" -O "$2"; }
+else
+	oops "você não tem wget ou curl instalado, o que eu preciso para baixar os arquivos"
+fi
+
+for f in "${files[@]}"
+do
+	echo "downloading $f from '$url' to '$tmpDir'..."
+	cmdfetch "$url/$f" "$tmpDir/$f" || oops "falha no download '$url/$f'"
+done
+
+} # fim do wrapping
